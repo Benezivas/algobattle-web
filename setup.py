c500188a0e54354b340a812c3c96b2184e200f93
Imogen <me@imogen.tech>
2022-10-06 14:34:54 +0200

    setup folder structure

---
+++ setup.py
@@ -0,0 +1,5 @@
+# Python is a bad language actually and needs this so we can install it in editable mode
+# with future pip releases this file might be obsolete
+
+import setuptools
+setuptools.setup()
