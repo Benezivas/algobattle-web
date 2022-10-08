f65a11373c6a3655fe5980952675ecfef4a10a55
Imogen <me@imogen.tech>
2022-10-08 17:27:23 +0200

    cleanup code

--- algobattle_web/login.py
+++ algobattle_web/login.py
@@ -1,12 +1,14 @@
 "Module specifying the login page."
 from __future__ import annotations
 from dataclasses import dataclass
+from datetime import timedelta, datetime
 from enum import Enum
 from uuid import UUID, uuid1
 from fastapi import APIRouter, Request, Form
 from fastapi.responses import HTMLResponse
 
 from algobattle_web.templates import templates as t
+from algobattle_web.util import send_email
 
 router = APIRouter(prefix="/login", tags=["login"])
 
@@ -19,32 +21,18 @@ class User:
     def __post_init__(self):
         self.id = uuid1()
 
-
 db: dict[UUID, User] = {}
-
-
 def add_user(user: User) -> None:
     db[user.id] = user
 add_user(User("me@me"))
 
 
-def user_exists(email: str) -> bool:
-    return any(u.email == email for u in db.values())
 
 
 class LoginError(Enum):
     NoToken = 0
     InvalidToken = 1
     UnregisteredUser = 2
-
-
-def verify_token(token: str | None) -> str | LoginError:
-    if token is None:
-        return LoginError.NoToken
-    elif token == "correct":
-        return "yay"
-    else:
-        return LoginError.InvalidToken
 
 
 @router.get("", response_class=HTMLResponse)
@@ -59,14 +47,33 @@ async def login_get(request: Request, token: str | None = None):
 
 
 @router.post("", response_class=HTMLResponse)
-async def login_post(request: Request, email = Form()):
+async def login_post(request: Request, email: str = Form()):
     if user_exists(email):
-        # send login token to email
+        token = login_token(email)
+        send_email(email, f"{router.url_path_for('')}&token={token}")
         return t.TemplateResponse("login_email_sent.jinja", {"request": request})
     else:
         return t.TemplateResponse("login.jinja", {"request": request, "email": email, "error": LoginError.UnregisteredUser.value})
 
 
+def login_token(email: str, lifetime: timedelta = timedelta(hours=1)) -> str:
+    return f"{email}-{datetime.now() + lifetime}"
 
+
+def session_token(user: User):
+    return user.name
+
+
+def verify_token(token: str | None) -> str | LoginError:
+    if token is None:
+        return LoginError.NoToken
+    elif token == "correct":
+        return "yay"
+    else:
+        return LoginError.InvalidToken
+
+
+def user_exists(email: str) -> bool:
+    return any(u.email == email for u in db.values())
 
 
