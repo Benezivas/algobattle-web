8cef4ccbc6fe85c6de18a67587b4d682d1832b4b
Imogen <me@imogen.tech>
2022-10-08 23:39:24 +0200

    set auth token

--- algobattle_web/login.py
+++ algobattle_web/login.py
@@ -1,6 +1,6 @@
 "Module specifying the login page."
 from __future__ import annotations
-from dataclasses import dataclass
+from dataclasses import field, dataclass
 from datetime import timedelta, datetime
 from enum import Enum
 from typing import cast
@@ -22,16 +22,23 @@ ALGORITHM = "HS256"
 class User:
     email: str
     name: str | None = None
-    token_id: UUID | None = None
-
-    def __post_init__(self):
-        self.id = uuid1()
+    id: UUID = field(default_factory=uuid1)
+    token_id: UUID = field(default_factory=uuid1)
 
 db: dict[UUID, User] = {}
 def add_user(user: User) -> None:
     db[user.id] = user
 add_user(User("me@me"))
 
+def user_exists(email: str) -> bool:
+    return any(u.email == email for u in db.values())
+
+def get_user(email:str) -> User | None:
+    users = [u for u in db.values() if u.email == email]
+    if users:
+        return users[0]
+    else:
+        return None
 
 
 
@@ -45,9 +52,10 @@ class LoginError(Enum):
 @router.get("", response_class=HTMLResponse)
 async def login_get(request: Request, token: str | None = None):
     res = verify_login_token(token)
-    if isinstance(res, str):
-        # give session token
-        return RedirectResponse("/")
+    if isinstance(res, User):
+        response = RedirectResponse("/")
+        response.set_cookie("auth", auth_token(res))
+        return response
     else:
         return t.TemplateResponse("login.jinja", {"request": request, "error": res.value})
 
@@ -64,31 +72,34 @@ async def login_post(request: Request, email: str = Form()):
 
 def login_token(email: str, lifetime: timedelta = timedelta(hours=1)) -> str:
     payload = {
-        "type": "login_token",
+        "type": "login",
         "email": email,
         "exp": datetime.now() + lifetime,
     }
     return jwt.encode(payload, JWT_SECRET, ALGORITHM)
 
 
-def verify_login_token(token: str | None) -> str | LoginError:
+def verify_login_token(token: str | None) -> User | LoginError:
     if token is None:
         return LoginError.NoToken
     try:
         payload = jwt.decode(token, JWT_SECRET, ALGORITHM)
-        if payload["type"] == "login_token":
-            return cast(str, payload["email"])
-        else:
-            return LoginError.InvalidToken
+        if payload["type"] == "login":
+            user = get_user(cast(str, payload["email"]))
+            if user is not None:
+                return user
     except ExpiredSignatureError:
         return LoginError.ExpiredToken
     except (JWTError, NameError):
-        return LoginError.InvalidToken
+        pass
+    return LoginError.InvalidToken
 
 
-def user_exists(email: str) -> bool:
-    return any(u.email == email for u in db.values())
-
-
-def session_token(user: User):
-    return user.name
+def auth_token(user: User):
+    payload = {
+        "type": "auth",
+        "user_id": str(user.id),
+        "token_id": str(user.token_id),
+        "exp": datetime.now() + timedelta(weeks=4)
+    }
+    return jwt.encode(payload, JWT_SECRET, ALGORITHM)
