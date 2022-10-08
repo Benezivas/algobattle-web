0a3ff755d2e99685ffa8a230f4a3ef701fe33cf7
Imogen <me@imogen.tech>
2022-10-08 13:09:46 +0200

    cleanup

--- algobattle_web/login.py
+++ algobattle_web/login.py
@@ -1,12 +1,35 @@
 "Module specifying the login page."
 from __future__ import annotations
+from dataclasses import dataclass
 from enum import Enum
+from uuid import UUID, uuid1
 from fastapi import APIRouter, Request, Form
 from fastapi.responses import HTMLResponse
 
-from algobattle_web.templates import templates
+from algobattle_web.templates import templates as t
 
 router = APIRouter(prefix="/login", tags=["login"])
+
+
+@dataclass
+class User:
+    email: str
+    name: str | None = None
+
+    def __post_init__(self):
+        self.id = uuid1()
+
+
+db: dict[UUID, User] = {}
+
+
+def add_user(user: User) -> None:
+    db[user.id] = user
+add_user(User("me@me"))
+
+
+def user_exists(email: str) -> bool:
+    return any(u.email == email for u in db.values())
 
 
 class LoginError(Enum):
@@ -18,29 +41,30 @@ class LoginError(Enum):
 def verify_token(token: str | None) -> str | LoginError:
     if token is None:
         return LoginError.NoToken
-    if token == "correct":
+    elif token == "correct":
         return "yay"
     else:
         return LoginError.InvalidToken
-
-def user_exists(email: str) -> bool:
-    return email == "me@e"
 
 
 @router.get("", response_class=HTMLResponse)
 async def login_get(request: Request, token: str | None = None):
     res = verify_token(token)
     if isinstance(res, str):
-        return "test"
+        # give session token
+        # redirect to user page
+        return "user page"
     else:
-        return templates.TemplateResponse("login.jinja", {"request": request, "error": res.value})
+        return t.TemplateResponse("login.jinja", {"request": request, "error": res.value})
+
 
 @router.post("", response_class=HTMLResponse)
 async def login_post(request: Request, email = Form()):
     if user_exists(email):
-        return templates.TemplateResponse("login_email_sent.jinja", {"request": request})
+        # send login token to email
+        return t.TemplateResponse("login_email_sent.jinja", {"request": request})
     else:
-        return templates.TemplateResponse("login.jinja", {"request": request, "email": email, "error": LoginError.UnregisteredUser.value})
+        return t.TemplateResponse("login.jinja", {"request": request, "email": email, "error": LoginError.UnregisteredUser.value})
 
 
 
