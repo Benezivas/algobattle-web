29f4f1f0fc5fc4e0d9ff2084405bb5cb702c22af
Imogen <me@imogen.tech>
2022-10-11 23:52:41 +0200

    use templated in home

--- algobattle_web/routers/home.py
+++ algobattle_web/routers/home.py
@@ -1,18 +1,18 @@
 "Module specifying the home page."
 from __future__ import annotations
 
-from fastapi import APIRouter, Request, Depends
+from fastapi import APIRouter
 from fastapi.responses import HTMLResponse
 
-from algobattle_web.templates import templates as t
-from algobattle_web.models.user import User, curr_user
+from algobattle_web.templates import templated
 
 router = APIRouter(tags=["home"])
 
 
 @router.get("/", response_class=HTMLResponse)
-async def home_get(request: Request, user: User = Depends(curr_user)):
-    return t.TemplateResponse("home.jinja", {"request": request, "user": user.name})
+@templated
+async def home_get():
+    return "home.jinja"
 
 
 
