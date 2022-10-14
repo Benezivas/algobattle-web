ffbb4028a1800837a3037c8020f7ed2ab2f628c3
Imogen <me@imogen.tech>
2022-10-14 13:35:36 +0200

    make toggle admin button functional

--- algobattle_web/routers/admin/users.py
+++ algobattle_web/routers/admin/users.py
@@ -1,10 +1,12 @@
 "Module specifying the admin user control page."
 from __future__ import annotations
-from fastapi import APIRouter, Depends
+from uuid import UUID
+from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.responses import HTMLResponse
 from algobattle_web.database import get_db, Session
-from algobattle_web.models.user import User
+from algobattle_web.models.user import User, get_user
 from algobattle_web.templates import templated
+from algobattle_web.util import BaseSchema
 
 router = APIRouter(prefix="/users", tags=["users"])
 
@@ -15,4 +17,17 @@ async def user_get(db: Session = Depends(get_db)):
     users = db.query(User).order_by(User.is_admin).all()
     return "admin_users.jinja", {"users": reversed(users)}
 
+
+class UserAdmin(BaseSchema):
+    id: UUID
+    make_admin: bool
+
+@router.post("/setadmin")
+async def make_admin(*, db: Session = Depends(get_db), user: UserAdmin):
+    user_db = get_user(db, user.id)
+    if user_db is not None:
+        user_db.is_admin = user.make_admin
+        db.commit()
+    else:
+        raise HTTPException(status.HTTP_400_BAD_REQUEST)
 
---
+++ algobattle_web/static/scripts/admin_users.js
@@ -0,0 +1,54 @@
+
+function bool(stringValue) {
+    switch(stringValue.toLowerCase().trim()){
+        case "true": 
+        case "yes": 
+        case "1": 
+          return true;
+
+        case "false": 
+        case "no": 
+        case "0":
+        case "none":
+        case null: 
+        case undefined:
+          return false;
+
+        default: 
+          return JSON.parse(stringValue);
+    }
+}
+
+async function toggle_admin(event) {
+    var row = event.currentTarget.closest("tr")
+    var data = row.dataset;
+    var response = await fetch("/admin/users/setadmin", {
+        "method": "POST",
+        "headers": {"Content-type": "application/json"},
+        "body": JSON.stringify({"id": data.uid, "make_admin": !bool(data.is_admin)}),
+    });
+    if (response.ok) {
+        data.is_admin = !bool(data.is_admin);
+
+        var admin_button_icon = row.children[3].children[0].children[0].children[0];
+        if (bool(data.is_admin)) {
+            admin_button_icon.classList.replace("bi-patch-plus", "bi-patch-minus");
+        } else {
+            admin_button_icon.classList.replace("bi-patch-minus", "bi-patch-plus");
+        }
+        if (bool(data.is_admin)) {
+            var check_icon = document.getElementById("admin-check").content.cloneNode(true);
+            row.children[0].appendChild(check_icon);
+        } else {
+            var check_icon = row.children[0].children[0];
+            row.children[0].removeChild(check_icon);
+        }
+    }
+    
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    for (var button of document.getElementsByClassName("user-toggle-admin")) {
+        button.addEventListener("click", toggle_admin);
+    }    
+})
