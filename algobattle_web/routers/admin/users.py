f890a9ad842cc7a70cff93d93a7cf4d3ec24122c
Imogen <me@imogen.tech>
2022-10-18 02:52:19 +0200

    add create user to admin panel

--- algobattle_web/routers/admin/users.py
+++ algobattle_web/routers/admin/users.py
@@ -1,11 +1,11 @@
 "Module specifying the admin user control page."
 from __future__ import annotations
 from uuid import UUID
-from fastapi import APIRouter, Depends, HTTPException, status
-from fastapi.responses import HTMLResponse
+from fastapi import APIRouter, Depends, HTTPException, status, Form
+from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.encoders import jsonable_encoder
 from algobattle_web.database import get_db, Session
-from algobattle_web.models.user import User, get_user, update_user
+from algobattle_web.models.user import User, create_user, get_user, update_user, UserCreate
 from algobattle_web.templates import templated
 from algobattle_web.util import BaseSchema
 
@@ -33,3 +33,10 @@ async def edit_user(*, db: Session = Depends(get_db), edit: EditUser):
         raise HTTPException(status.HTTP_400_BAD_REQUEST)
     update_user(db, user, edit.email, edit.name, edit.is_admin)
     return user
+
+
+@router.post("/create", response_class=RedirectResponse)
+async def users_create(*, db: Session = Depends(get_db), name: str = Form(), email: str = Form(), is_admin: bool = Form(default=False)):
+    create = UserCreate(email=email, name=name, is_admin=is_admin )
+    create_user(db, create)
+    return RedirectResponse("/admin/users", status_code=status.HTTP_302_FOUND)
