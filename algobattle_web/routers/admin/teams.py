9c387e68ab8f7c9d2fd73281c6b0a934bfbedf03
Imogen <me@imogen.tech>
2022-10-20 00:47:44 +0200

    add adding teams to site

--- algobattle_web/routers/admin/teams.py
+++ algobattle_web/routers/admin/teams.py
@@ -4,8 +4,9 @@ from uuid import UUID
 from fastapi import APIRouter, Depends, HTTPException, status, Form
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.encoders import jsonable_encoder
+
 from algobattle_web.database import get_db, Session
-from algobattle_web.models.team import Team
+from algobattle_web.models.team import Team, create_context, Context
 from algobattle_web.templates import templated
 
 router = APIRouter(prefix="/teams", tags=["teams"])
@@ -14,7 +15,12 @@ router = APIRouter(prefix="/teams", tags=["teams"])
 @router.get("", response_class=HTMLResponse)
 @templated
 async def user_get(db: Session = Depends(get_db)):
-    teams = db.query(Team).all()
-    teams = jsonable_encoder(teams)[::-1]
-    return "admin_teams.jinja", {"teams": teams}
+    teams = jsonable_encoder(db.query(Team).all())
+    contexts = jsonable_encoder(db.query(Context).all()) 
+    return "admin_teams.jinja", {"teams": teams, "contexts": contexts}
 
+
+@router.post("/create_context", response_class=RedirectResponse)
+async def context_create(*, db: Session = Depends(get_db), name: str = Form()):
+    create_context(db, name)
+    return RedirectResponse("/admin/teams", status_code=status.HTTP_302_FOUND)
