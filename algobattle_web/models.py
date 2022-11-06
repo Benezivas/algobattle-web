930ee6c750b3f28475467d0f6879fe4af09682b5
Imogen <me@imogen.tech>
2022-11-07 00:17:48 +0100

    fix UserSettings

--- algobattle_web/models.py
+++ algobattle_web/models.py
@@ -123,14 +123,14 @@ class User(Base):
 
 class UserSettings(Base):
     __tablename__ = "usersettings"  # type: ignore
-    user_id: Mapped[ID] = mapped_column(ForeignKey("users.id"))
-    selected_team_id: Mapped[ID | None] = mapped_column(ForeignKey("teams.id"), default=None)
+    user_id: Mapped[UUID] = mapped_column(ForeignKey("users.id"))
+    selected_team_id: Mapped[UUID | None] = mapped_column(ForeignKey("teams.id"), default=None)
 
     user: Mapped[User] = relationship(back_populates="settings", lazy="joined")
     selected_team: Mapped[Team | None] = relationship(lazy="joined")
 
     class Schema(Base.Schema):
-        selected_team: ObjID
+        selected_team: ObjID | None
 
 
 class Context(Base):
