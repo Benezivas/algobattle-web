a166a4e2c3141b24406f03e14bd0c0b72ef35be5
Imogen <me@imogen.tech>
2023-01-08 01:22:09 +0100

    update battle execution

--- algobattle_web/battle.py
+++ algobattle_web/battle.py
@@ -8,9 +8,11 @@ from zipfile import ZipFile
 from sqlalchemy import select
 from sqlalchemy_media import StoreManager
 
-from algobattle.team import TeamInfo
+from algobattle.team import TeamInfo, TeamHandler
 from algobattle.match import Match
 from algobattle.util import TempDir
+from algobattle.problem import Problem
+from algobattle.cli import Config
 from algobattle_web.models import MatchResult, Program, ResultParticipantInfo, Schedule
 from algobattle_web.database import DbFile, Session
 from algobattle_web.config import STORAGE_PATH
@@ -45,9 +47,9 @@ def run_match(db: Session, scheduled_match: Schedule):
     with TempDir() as folder:
         logger, logging_path = _setup_logging(folder, verbose=True, silent=True)
         if scheduled_match.config is None:
-            config = scheduled_match.problem.config
+            config_db = scheduled_match.problem.config
         else:
-            config = scheduled_match.config
+            config_db = scheduled_match.config
         problem_path = _extract_to(STORAGE_PATH / scheduled_match.problem.file.path, folder / "problem")
             
         participants = []
@@ -58,38 +60,33 @@ def run_match(db: Session, scheduled_match: Schedule):
             else:
                 gen = participant.gen_prog
             assert gen is not None
-            gen_path = _extract_to(STORAGE_PATH / gen.file.path, STORAGE_PATH / "tmp" / participant.team.name / "generator")
+            gen_path = _extract_to(STORAGE_PATH / gen.file.path, folder / participant.team.name / "generator")
             
             if participant.solver.src == "team_spec":
                 sol = db.scalars(select(Program).where(Program.team_id == participant.team_id, Program.role == "solver")).unique().first()
             else:
                 sol = participant.sol_prog
             assert sol is not None
-            sol_path = _extract_to(STORAGE_PATH / sol.file.path, STORAGE_PATH / "tmp" / participant.team.name / "solver")
+            sol_path = _extract_to(STORAGE_PATH / sol.file.path, folder / participant.team.name / "solver")
 
             participants.append(ResultParticipantInfo(participant.team, 0, generator=gen, solver=sol))
             team_infos.append(TeamInfo(participant.team.name, generator=gen_path, solver=sol_path))
-        db_result = MatchResult.create(db, schedule=scheduled_match, config=config, status="running", participants=participants)
+        db_result = MatchResult.create(db, schedule=scheduled_match, config=config_db, status="running", participants=participants)
         db.commit()
 
-        with MatchInfo.build(
-            problem_path=problem_path,
-            config_path = STORAGE_PATH / config.file.path,
-            team_infos=team_infos,
-            battle_type="iterated",
-            rounds=2,
-        ) as match_info:
-            result = match_info.run_match()
-            points = result.calculate_points(scheduled_match.points)
+        config = Config.from_file(Path(config_db.file.path))
+        problem = Problem.import_from_path(problem_path)
+        with TeamHandler.build(team_infos, problem, config.docker, config.execution.safe_build) as teams:
+            result = Match.run(config.match, config.battle_config, problem, teams)
+            points = result.calculate_points()
             
             logger.info('#' * 78)
             logger.info(str(result))
             for team, pts in points.items():
                 logger.info(f"Team {team} gained {pts:.1f} points.")
 
-            team_table = {team.name: team for team in match_info.teams}
             for participant in db_result.participants:
-                participant.points = points[team_table[participant.team.name]]
+                participant.points = points[participant.team.name]
             db_result.status = "complete"
             logging.shutdown()
             with StoreManager(db), open(logging_path, "rb") as logs:
