2de0d43b1064ef14c4d834288b5931b38d3d6327
Imogen <me@imogen.tech>
2023-01-08 11:31:08 +0100

    clenaup imports

--- algobattle_web/battle.py
+++ algobattle_web/battle.py
@@ -2,8 +2,6 @@ from __future__ import annotations
 from datetime import datetime
 import logging
 from pathlib import Path
-import shutil
-import sys
 from zipfile import ZipFile
 from sqlalchemy import select
 from sqlalchemy_media import StoreManager
