dcc76fd103a056d05211af1d58c4cd4cebb87fb9
Imogen <me@imogen.tech>
2023-01-31 00:33:19 +0100

    add exception handler to more nicely display validation errors

--- algobattle_web/__init__.py
+++ algobattle_web/__init__.py
@@ -1,7 +1,10 @@
 from __future__ import annotations
 from pathlib import Path
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse
 from fastapi.staticfiles import StaticFiles
+from fastapi.encoders import jsonable_encoder
 from uvicorn import run
 
 from algobattle_web.database import Base, SessionLocal, engine
@@ -15,10 +18,24 @@ from algobattle_web.pages import router as pages
 Base.metadata.create_all(bind=engine)
 with SessionLocal() as db:
     if User.get(db, ADMIN_EMAIL) is None:
-        User.create(db, email=ADMIN_EMAIL, name="Admin", is_admin=True)
+        User(db, email=ADMIN_EMAIL, name="Admin", is_admin=True)
+        db.commit()
 
 
 app = FastAPI()
+
+
+@app.exception_handler(RequestValidationError)
+async def err_handler(request: Request, e: RequestValidationError):
+    return JSONResponse(
+        status_code=422,
+        content=jsonable_encoder({
+            "detail": e.errors(),
+            "body": e.body,
+        })
+    )
+
+
 app.include_router(api)
 app.include_router(pages)
 
