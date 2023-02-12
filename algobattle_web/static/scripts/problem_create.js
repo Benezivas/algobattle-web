2fc0de4b4a27637c8e6517d3a8110ac5dddbcd88
Imogen <me@imogen.tech>
2023-02-12 15:41:50 +0100

    file

--- algobattle_web/static/scripts/problem_create.js
+++ algobattle_web/static/scripts/problem_create.js
@@ -10,8 +10,32 @@ const app = createApp({
     data() {
         return {
             store: store,
-            form_index: 2,
+            form_index: 0,
+
+            name: null,
+            description: null,
+            problem_schema: null,
+            solution_schema: null,
+
+            context: null,
+            start: null,
+            end: null,
+
+            image: null,
+            alt: null,
+            short_description: null,
+
         }
+    },
+    methods:  {
+        async send_file(event) {
+            const form = new FormData(event.currentTarget)
+            const reponse = await send_form("/api/problem/", form)
+            if (response.ok) {
+                this.form_index = 1
+            }
+            send_form(event)
+        },
     },
     computed: {
     },
