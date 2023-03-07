ee2a2cb7bb14b9584fc810e5da3cf23cf725863d
Imogen <me@imogen.tech>
2023-03-07 20:35:25 +0100

    add global user object

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,10 +1,14 @@
-import { createApp } from 'vue'
+import { createApp, reactive } from 'vue'
 import App from './App.vue'
 import router from './router'
 
 import "./assets/styles.scss"
 import * as bootsrap from "bootstrap"
 import './assets/main.css'
+
+const store = reactive({
+    user: {},
+})
 
 const app = createApp(App)
 
