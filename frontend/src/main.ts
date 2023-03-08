79ae6e25bea48fc4b7bb1bec0329b255694d8d2e
Imogen <me@imogen.tech>
2023-03-08 13:34:11 +0100

    fix typo

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -5,16 +5,17 @@ import router from './router'
 
 import "./assets/styles.scss"
 import { Configuration, UserApi } from "../typescript_client"
+import type { AlgobattleWebModelsUserSchema } from "../typescript_client"
 
 const configuration = new Configuration({
     basePath: "http://127.0.0.1:8000",
 })
 
-const userApi = new UserApi(configuration)
+export const userApi = new UserApi(configuration)
 
 
 export const store = reactive({
-    user:  await userApi.getSelf(),
+    user: {} as AlgobattleWebModelsUserSchema,
 })
 
 const app = createApp(App)
