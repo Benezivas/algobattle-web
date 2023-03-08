5ca44c410cdb24552e49310f36caa8bc0280e7db
Imogen <me@imogen.tech>
2023-03-08 23:00:14 +0100

    remove autogenerated views

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -5,17 +5,17 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
+      path: "/",
+      name: "home",
       component: HomeView
     },
     {
-      path: '/about',
-      name: 'about',
+      path: "/problems",
+      name: "problems",
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      component: () => import('../views/ProblemView.vue')
     }
   ]
 })
