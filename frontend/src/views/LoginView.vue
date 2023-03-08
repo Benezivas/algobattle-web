f018c767a6d36a9411c0e0a29ba507f2986c8e36
Imogen <me@imogen.tech>
2023-03-08 19:03:27 +0100

    add login page

---
+++ frontend/src/views/LoginView.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import { userApi } from '@/main';
+import { ref } from 'vue';
+import { useRoute } from 'vue-router';
+
+const route = useRoute()
+var email = ref("")
+var msg = ref("")
+
+async function login() {
+  try {
+    await userApi.login({bodyLoginApiUserLoginPost: {email: email.value, targetUrl: route.fullPath}})
+    msg.value = "email_sent"
+  } catch {
+    msg.value = "error"
+  }
+}
+
+</script>
+
+<template>
+  <h1>Login</h1>
+  <div v-if="msg == 'error'" class="alert alert-danger" role="alert">
+    There was an error logging you in.
+    <button type="button" class="btn-close float-end" data-bs-dismiss="alert" aria-label="Close"></button>
+  </div>
+  <div v-if="msg == 'email_sent'" class="alert alert-success" role="alert">
+    We've sent you an email, click the link in it to login.
+  </div>
+  <form @submit.prevent="login">
+    <div class="mb-3">
+      <label for="email" class="form-label">Email address</label>
+      <input type="email" class="form-control w-em" id="email" required v-model="email">
+    </div>
+    <button type="submit" class="btn btn-primary">Login</button>
+  </form>
+</template>
