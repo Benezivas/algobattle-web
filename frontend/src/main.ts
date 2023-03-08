dbafbec6096668f02aee37b3fab402d443520872
Imogen <me@imogen.tech>
2023-03-08 22:46:11 +0100

    send api token from frontend

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,13 +2,16 @@ import * as bootstrap from "bootstrap"
 import { createApp, reactive } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { useCookies } from "vue3-cookies"
 
 import "./assets/styles.scss"
 import { Configuration, UserApi } from "../typescript_client"
 import type { AlgobattleWebModelsUserSchema } from "../typescript_client"
 
+const { cookies } = useCookies()
 const configuration = new Configuration({
     basePath: "http://127.0.0.1:8000",
+    apiKey: () => cookies.get("user_token"),
 })
 
 export const userApi = new UserApi(configuration)
