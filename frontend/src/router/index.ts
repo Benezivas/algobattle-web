11c09e076d0b9e50abaa42449e11fb6a7f8d36f7
Imogen <me@imogen.tech>
2023-03-09 01:03:29 +0100

    add problem detail route

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -16,7 +16,11 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/ProblemView.vue')
-    }
+    },
+    {
+        path: "/problems/:contextName/:problemName",
+        component: () => import("../views/ProblemDetail.vue")
+    },
   ]
 })
 
