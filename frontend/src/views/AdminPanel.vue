a91d5c4c19ebc41c923dc7c1c5360dd65e485524
Imogen <me@imogen.tech>
2023-03-12 17:36:58 +0100

    add admin panel

--- frontend/src/views/AdminPanel.vue
+++ frontend/src/views/AdminPanel.vue
@@ -1,7 +1,26 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+import AdminUsers from "./AdminUsers.vue"
+import AdminTeams from "./AdminTeams.vue"
+import AdminContexts from "./AdminContexts.vue"
 
+const tab = ref("users")
 </script>
 
 <template>
+  <ul class="nav nav-tabs">
+    <li class="nav-item">
+      <button class="nav-link" :class="{active: tab == 'users'}" :aria-current="tab == 'users'" @click="e => tab = 'users'">Users</button>
+    </li>
+    <li class="nav-item">
+      <button class="nav-link" :class="{active: tab == 'teams'}" :aria-current="tab == 'teams'" @click="e => tab = 'teams'">Teams</button>
+    </li>
+    <li class="nav-item">
+      <button class="nav-link" :class="{active: tab == 'contexts'}" :aria-current="tab == 'contexts'" @click="e => tab = 'contexts'">Contexts</button>
+    </li>
+  </ul>
 
+  <AdminUsers v-if="tab == 'users'" />
+  <AdminTeams v-if="tab == 'teams'" />
+  <AdminContexts v-if="tab == 'contexts'" />
 </template>
