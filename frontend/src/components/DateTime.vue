f2b1aca04c33e41e9d1044ac3ef232fbb14dceaa
Imogen <me@imogen.tech>
2023-07-14 00:37:26 +0200

    change datetimes to be in server's timezone

--- frontend/src/components/DateTime.vue
+++ frontend/src/components/DateTime.vue
@@ -8,12 +8,17 @@ const emit = defineEmits<{
 }>();
 
 function onInput(event: any) {
-  emit("update:modelValue", event.target?.value ? new Date(event.target.value) : undefined);
+  let date = new Date(event.target.value);
+  // we convert it to UTC here, this way all timestamps are in UTC everywhere
+  // the frontend displays them as ISO strings, i.e. in UTC, and the backend treats them as timezone unaware
+  // thus we can always enter times and have them be treated as being in the server's local timezone
+  date = new Date(date.getTime() - (date.getTimezoneOffset()*60*1000));
+  emit("update:modelValue", event.target?.value ? date : undefined);
 }
 function format(date: Date | undefined) {
   // this seems to actually be the best way to do this
   // https://stackoverflow.com/q/23593052/1850609
-  return date && new Date(date.getTime()-(date.getTimezoneOffset()*60*1000)).toISOString().split("Z")[0];
+  return date && new Date(date.getTime()).toISOString().split("Z")[0];
 }
 </script>
 
