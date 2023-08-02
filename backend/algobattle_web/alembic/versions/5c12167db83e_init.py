4e1d951b8ef6c5c96c109c64e710c91e91c4eaee
Imogen <me@imogen.tech>
2023-08-02 19:29:49 +0200

    add initial db version

---
+++ backend/algobattle_web/alembic/versions/5c12167db83e_init.py
@@ -0,0 +1,22 @@
+"""init
+
+Revision ID: 5c12167db83e
+Revises: 
+Create Date: 2023-08-02 19:29:11.058661
+
+"""
+
+
+# revision identifiers, used by Alembic.
+revision = '5c12167db83e'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
