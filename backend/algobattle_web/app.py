f263096315b993e816a15047c3c427e69df9dd00
Imogen <me@imogen.tech>
2023-08-02 19:24:41 +0200

    auto update database schema on server startup

--- backend/algobattle_web/app.py
+++ backend/algobattle_web/app.py
@@ -1,5 +1,6 @@
 from contextlib import asynccontextmanager
 import json
+from importlib.resources import as_file, files
 
 from fastapi import FastAPI, Request, status
 from fastapi.exceptions import RequestValidationError, HTTPException
@@ -9,6 +10,9 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi.openapi.utils import get_openapi
 from sqlalchemy_utils.functions import database_exists, create_database
 from sqlalchemy import create_engine
+from alembic.config import Config
+from alembic.command import upgrade, stamp
+from alembic.migration import MigrationContext
 
 from algobattle_web.models import Base, User
 from algobattle_web.api import router as api, SchemaRoute
@@ -24,9 +28,26 @@ async def lifespan(app: FastAPI):
     engine = create_engine(ServerConfig.obj.database_url)
     SessionLocal.configure(bind=engine)
 
+    # this creates the database itself, alembic/sqlalchemy code below creates the tables in it
     if not database_exists(engine.url):
         create_database(engine.url)
-    Base.metadata.create_all(bind=engine)
+
+    # because python packaged may be installed to eg zipfiles we need make sure all the data is actually on disk
+    # however, that isn't easy here since alembic (presumably) expects a bunch of files in a certain structure.
+    # this code's invocation of alembic will (probably) just break if you use esoteric install options 🤷‍♀️
+    data_files = files("algobattle_web.alembic")
+    with (as_file(data_files / "alembic.ini") as alembic_ini):
+        alembic_cfg = Config(alembic_ini)
+        alembic_cfg.set_main_option("script_location", str(alembic_ini.parent))
+        with engine.connect() as connection:
+            context = MigrationContext.configure(connection)
+            if context.get_current_revision() is None:
+                Base.metadata.create_all(bind=engine)
+                stamp(alembic_cfg, "head")
+            else:
+                upgrade(alembic_cfg, "head")
+
+    # create the default admin user
     with SessionLocal() as db:
         if User.get(db, ServerConfig.obj.admin_email) is None:
             User(db, email=ServerConfig.obj.admin_email, name="Admin", is_admin=True)
