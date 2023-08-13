133e2cc76a3a77c0daad3c0dc38e9ed5c7ba5b68
Imogen <me@imogen.tech>
2023-08-14 00:30:25 +0200

    fix program store

--- frontend/src/views/Results.vue
+++ frontend/src/views/Results.vue
@@ -1,24 +1,27 @@
 <script setup lang="ts">
 import { store, type ModelDict, formatDateTime } from "@/main";
-import { MatchService, TournamentService, ProblemService, MatchStatus, ProgramService, Role } from "@client";
+import { MatchService, TournamentService, ProblemService, MatchStatus, ProgramService, Role, TeamService } from "@client";
 import { Modal } from "bootstrap";
 import type { Problem, Tournament, MatchResult, Team, DbFile, ResultParticipant, Program } from "@client";
 import { onMounted, ref, toRaw } from "vue";
 import DownloadButtonVue from "@/components/DownloadButton.vue";
 
 const problems = ref<ModelDict<Problem>>({});
-const tournaments = ref<ModelDict<Tournament>>({});
 const results = ref<ModelDict<MatchResult>>({});
 const teams = ref<ModelDict<Team>>({});
-const programs = ref<{ [team_id: string]: { [role in Role]: Program[] } }>({});
+const programs = ref<{ [key: string]: Program[] }>({});
 
 let editModal: Modal;
 onMounted(async () => {
-  tournaments.value = await TournamentService.allTournaments();
   const res = await MatchService.results();
   problems.value = res.problems;
   results.value = res.results;
   teams.value = res.teams;
+  if (store.user.is_admin) {
+    const res = await TeamService.searchTeam({tournament: store.user.current_tournament?.id});
+    // TODO: get all teams not just first page
+    teams.value = res.teams;
+  }
   editModal = Modal.getOrCreateInstance("#editModal");
   if (store.user.is_admin) {
     problems.value = await ProblemService.allProblems({
@@ -60,16 +63,26 @@ async function sendData() {}
 async function deleteResult() {}
 
 function getPrograms(team: string, role: Role) {
-  if (programs.value[team] && programs.value[team][role]) {
+  if (!editData.value.problem || programs.value[editData.value.problem + team + role]) {
     return;
   }
+  programs.value[editData.value.problem + team + role] = [];
   ProgramService.searchProgram({
     team: team,
     role: role,
     problem: editData.value.problem,
   }).then((response) => {
-    programs.value[team][role] = Object.values(response.programs);
+    programs.value[editData.value.problem + team + role] = Object.values(response.programs);
   });
+}
+
+function getAllPrograms() {
+  for (const participant of editData.value.participants) {
+    if (participant.team_id) {
+      getPrograms(participant.team_id, Role.GENERATOR);
+      getPrograms(participant.team_id, Role.SOLVER);
+    }
+  }
 }
 </script>
 
@@ -118,7 +131,7 @@ function getPrograms(team: string, role: Role) {
           <label for="time" class="form-label">Time</label>
           <input id="time" class="form-control" type="datetime-local" required v-model="editData.time" />
           <label for="problem" class="form-label">Problem</label>
-          <select id="problem" class="form-select" required v-model="editData.problem">
+          <select id="problem" class="form-select" required v-model="editData.problem" @click="getAllPrograms">
             <option v-for="(problem, id) in problems" :value="id">{{ problem.name }}</option>
           </select>
           <label for="status" class="form-label">Status</label>
@@ -153,28 +166,28 @@ function getPrograms(team: string, role: Role) {
                 </td>
                 <td>
                   <select
-                    v-if="participant.team_id"
+                    v-if="participant.team_id && editData.problem"
                     :id="participant.team_id + 'gen'"
                     class="form-select"
                     required
                     v-model="participant.generator"
                     @click="getPrograms(participant.team_id, Role.GENERATOR)"
                   >
-                    <option v-for="prog in programs[participant.team_id][Role.GENERATOR]" :value="prog">
+                    <option v-for="prog in programs[editData.problem + participant.team_id + Role.GENERATOR]" :value="prog">
                       {{ `${prog.name}(${formatDateTime(prog.creation_time)})` }}
                     </option>
                   </select>
                 </td>
                 <td>
                   <select
-                    v-if="participant.team_id"
+                    v-if="participant.team_id && editData.problem"
                     :id="participant.team_id + 'gen'"
                     class="form-select"
                     required
                     v-model="participant.solver"
                     @click="getPrograms(participant.team_id, Role.SOLVER)"
                   >
-                    <option v-for="prog in programs[participant.team_id][Role.SOLVER]" :value="prog">
+                    <option v-for="prog in programs[editData.problem + participant.team_id + Role.SOLVER]" :value="prog">
                       {{ `${prog.name}(${formatDateTime(prog.creation_time)})` }}
                     </option>
                   </select>
