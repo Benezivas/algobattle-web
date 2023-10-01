f0094c5a5a9a3f414147548f0c9811d411151ad8
Imogen <me@imogen.tech>
2023-10-02 00:37:41 +0200

    fix settings page

--- frontend/src/views/Settings.vue
+++ frontend/src/views/Settings.vue
@@ -1,34 +1,18 @@
 <script setup lang="ts">
-import { type ModelDict, store } from "@/main";
-import { TeamService, TournamentService, UserService } from "@client";
-import type { Team, Tournament } from "@client";
-import { onMounted, ref } from "vue";
+import { store } from "@/main";
+import { UserService } from "@client";
+import { ref } from "vue";
 
 const userEdit = ref({
-  email: store.user.email,
-  selectedTeam: store.user.selected_team?.id,
-  selectedTournament: store.user.selected_tournament?.id,
+  email: store.user?.email,
 });
-const teams = ref<ModelDict<Team>>({});
-const tournaments = ref<{ [key: string]: Tournament }>({});
 const error = ref("");
-
-onMounted(async () => {
-  teams.value = await TeamService.getTeams({ requestBody: store.user.teams });
-  if (store.user.is_admin) {
-    tournaments.value = await TournamentService.allTournaments();
-  }
-});
 
 async function saveEdit() {
   const newUser = await UserService.settings({
-    requestBody: {
       email: userEdit.value.email,
-      team: userEdit.value.selectedTeam,
-      tournament: userEdit.value.selectedTournament,
-    },
   });
-  Object.assign(store.user, newUser);
+  store.user = newUser;
   error.value = "success";
 }
 </script>
@@ -38,17 +22,5 @@ async function saveEdit() {
   <h3>User settings</h3>
   <label class="form-label" for="emailInput">Email address</label>
   <input type="email" class="form-control  mb-2" id="emailInput" v-model="userEdit.email" />
-  <label for="selectedTeam" class="form-label">Selected team</label>
-  <select class="form-select" name="selectedTeam" v-model="userEdit.selectedTeam">
-    <option v-for="(team, id) in teams" :value="id">
-      {{ team.name + ` (${tournaments[team.tournament]?.name})` }}
-    </option>
-  </select>
-  <template v-if="store.user.is_admin">
-    <label for="currTournament" class="form-label">Current Tournament</label>
-    <select class="form-select" name="currTournament" v-model="userEdit.selectedTournament">
-      <option v-for="(tournament, id) in tournaments" :value="id">{{ tournament.name }}</option>
-    </select>
-  </template>
   <button type="submit" class="btn btn-primary mt-4" @click="saveEdit">Save changes</button>
 </template>
