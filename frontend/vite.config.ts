24f4a4008de34d70df686d9b6c20c0c684834ae7
Imogen <me@imogen.tech>
2023-10-18 20:43:20 +0200

    rename dev services to improve caching

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -16,7 +16,7 @@ export default defineConfig({
     host: "0.0.0.0",
     proxy: {
         "/api": {
-            target: "http://backend:8000",
+            target: "http://dev-backend:8000",
         },
     },
     watch: {
