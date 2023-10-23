4e0216c27ba921d0404095d4ab7edd6558e8bb7d
Imogen <me@imogen.tech>
2023-10-24 01:20:43 +0200

    add home page differentiation for logged out users

---
+++ frontend/src/views/HomeDefault.vue
@@ -0,0 +1,6 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <main>Default page</main>
+</template>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,5 +1,20 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { store } from "@/shared";
+import { onMounted, ref } from "vue";
+import HomeDefault from "./HomeDefault.vue";
+import { SettingsService } from "@client";
+
+const home_page = ref<string | null>();
+onMounted(async () => {
+  home_page.value = await SettingsService.home();
+})
+
+</script>
 
 <template>
-  <main>Home page</main>
+  <div v-if="store.user">
+    <main>Home page</main>
+  </div>
+  <div v-else-if="(typeof home_page === 'string')" v-html="home_page"/>
+  <HomeDefault v-else-if="home_page === null" />
 </template>
